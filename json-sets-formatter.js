afc0759dbc58d2ee7f40c05cd6c76f62618c363e
Garth Braithwaite <garthdb@gmail.com>
2022-02-09 12:16:58 -0700

    fix: moved ref outside of value prop

--- json-sets-formatter.js
+++ json-sets-formatter.js
@@ -4,22 +4,21 @@ const isObject = (item) => {
   return typeof item === "object" && !Array.isArray(item) && item !== null;
 };
 
-const pathToObj = (pathAr, value) => {
-  return (res = pathAr.reduceRight((value, key) => ({ [key]: value }), value));
-};
+const pathToObj = (pathAr, value) =>
+  pathAr.reduceRight((value, key) => ({ [key]: value }), value);
 
 const formatter = ({ dictionary, platform, file, options }) => {
   let resultObj = {};
   dictionary.allTokens.forEach((token) => {
     if (isObject(token.value)) {
+      const ref = token.original.value;
       const value = {
-        ref: token.original.value,
         sets: {
           mobile: token.value.sets.mobile.value,
           desktop: token.value.sets.desktop.value,
         },
       };
-      resultObj = merge(resultObj, pathToObj(token.path, { value }));
+      resultObj = merge(resultObj, pathToObj(token.path, { ref, value }));
     } else {
       resultObj = merge(
         resultObj,
--- test/test.js
+++ test/test.js
@@ -1,13 +1,15 @@
 import StyleDictionary from "style-dictionary";
 import { JsonSetsFormatter } from "../index";
 
+import fs from "fs";
+
 StyleDictionary.registerFormat(JsonSetsFormatter);
 
 const config = {
   source: ["test/fixtures/refset.json"],
   platforms: {
     JSON: {
-      buildPath: "tests/tmp/",
+      buildPath: "test/tmp/",
       transforms: ["attribute/cti", "name/cti/kebab"],
       files: [
         {
@@ -26,144 +28,11 @@ const config = {
 test("ref to set should include all values", () => {
   const sd = StyleDictionary.extend(config);
   sd.buildAllPlatforms();
-  expect("true").toBe("true");
+  const expected = fs.readFileSync("./test/expected/refset.json", {
+    encoding: "utf8",
+  });
+  const result = fs.readFileSync("./test/tmp/refset.json", {
+    encoding: "utf8",
+  });
+  expect(JSON.parse(result)).toMatchObject(JSON.parse(expected));
 });
-
-// import transforms from "../source/index";
-// import createFilesConfig from "../source/build";
-// import StyleDictionary from "style-dictionary";
-// import helpers from "./helpers";
-// import fs from "fs";
-// import path from "path";
-// import { jest } from "@jest/globals";
-//
-// StyleDictionary.registerTransform(transforms.SetsNameTransform);
-// StyleDictionary.registerTransform(transforms.SetsAttributeTransform);
-// StyleDictionary.registerTransformGroup(transforms.SetsTransformGroup);
-//
-// const buildPath = "test/dist/css/";
-// const destination = "default.css";
-//
-// const StyleDictionaryTransformGroup = StyleDictionary.extend({
-//   source: ["test/fixtures/**/*.json"],
-//   platforms: {
-//     css: {
-//       transformGroup: "Sets",
-//       buildPath: buildPath,
-//       deep: true,
-//       sets: ["light", "dark"],
-//       defaultSet: "dark",
-//       prefix: "spectrum",
-//       files: createFilesConfig(["light", "dark"]),
-//     },
-//   },
-// });
-//
-// describe("Sets Transforms", () => {
-//   beforeAll(() => {
-//     StyleDictionaryTransformGroup.cleanAllPlatforms();
-//   });
-//   describe("SetsAttributeTransform", () => {
-//     it("should build and write file", () => {
-//       StyleDictionaryTransformGroup.buildPlatform("css");
-//       expect(
-//         helpers.fileExists(path.join(buildPath, destination))
-//       ).toBeTruthy();
-//     });
-//     it("should add sets attribute", () => {
-//       const StyleDictionaryTransformAttribute = StyleDictionary.extend({
-//         source: ["test/fixtures/**/*.json"],
-//         platforms: {
-//           css: {
-//             transforms: ["attribute/sets"],
-//             buildPath: buildPath,
-//             deep: true,
-//             sets: ["light", "dark"],
-//             prefix: "spectrum",
-//             files: createFilesConfig(["light", "dark"]),
-//           },
-//         },
-//       });
-//       const output = StyleDictionaryTransformAttribute.exportPlatform("css");
-//       expect(
-//         Object.keys(output.colors.gray["50"].sets.light.attributes)
-//       ).toContain("set");
-//     });
-//     it("should add sets attribute when attribute already exists", () => {
-//       const StyleDictionaryTransformAttribute = StyleDictionary.extend({
-//         source: ["test/fixtures/**/*.json"],
-//         platforms: {
-//           css: {
-//             transforms: [
-//               "name/cti/kebab",
-//               "name/sets",
-//               "attribute/cti",
-//               "attribute/sets",
-//             ],
-//             buildPath: buildPath,
-//             deep: true,
-//             sets: ["light", "dark"],
-//             prefix: "spectrum",
-//             files: createFilesConfig(["light", "dark"]),
-//           },
-//         },
-//       });
-//       const output = StyleDictionaryTransformAttribute.exportPlatform("css");
-//       expect(
-//         Object.keys(output.colors.gray["50"].sets.light.attributes)
-//       ).toContain("set");
-//     });
-//     it("should remove the sets term from the name", () => {
-//       const StyleDictionaryTransformAttribute = StyleDictionary.extend({
-//         source: ["test/fixtures/**/*.json"],
-//         platforms: {
-//           css: {
-//             transformGroup: "Sets",
-//             buildPath: buildPath,
-//             deep: true,
-//             sets: ["light", "dark"],
-//             prefix: "spectrum",
-//             files: createFilesConfig(["light", "dark"]),
-//           },
-//         },
-//       });
-//       const output = StyleDictionaryTransformAttribute.exportPlatform("css");
-//       expect(
-//         Object.keys(output.colors.gray["50"].sets.light.attributes)
-//       ).toContain("set");
-//     });
-//     it("should handle nested sets", () => {
-//       const StyleDictionaryTransformAttribute = StyleDictionary.extend({
-//         source: ["test/fixtures/**/*.json"],
-//         platforms: {
-//           css: {
-//             transformGroup: "Sets",
-//             buildPath: buildPath,
-//             deep: true,
-//             sets: [
-//               "light",
-//               "dark",
-//               "spectrum",
-//               "ccx",
-//               "mobile",
-//               "spectrum-mobile",
-//               "spectrum-desktop",
-//             ],
-//             prefix: "spectrum",
-//             files: createFilesConfig([
-//               "light",
-//               "dark",
-//               "spectrum",
-//               "ccx",
-//               "mobile",
-//               "spectrum-mobile",
-//               "spectrum-desktop",
-//             ]),
-//           },
-//         },
-//       });
-//       const output = StyleDictionaryTransformAttribute.exportPlatform("css");
-//       expect(Object.keys(output)).toContain("default");
-//     });
-//   });
-// });
