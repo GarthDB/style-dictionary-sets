370a332b15bcff5e36e5d78509f42e97db7de703
Garth Braithwaite <garthdb@gmail.com>
2022-02-17 18:15:04 -0500

    test: cleaned up unused helpers

--- tests/helpers.js
+++ tests/helpers.js
@@ -9,25 +9,5 @@ export default {
   },
   fileToJSON: (path) => {
     return fs.readJsonSync(path);
-  },
-  fileExists: (filePath) => {
-    try {
-      return fs.statSync(filePath).isFile();
-    } catch (err) {
-      return false;
-    }
-  },
-  pathDoesNotExist: (path) => {
-    try {
-      return !fs.existsSync(path);
-    } catch (err) {
-      return false;
-    }
-  },
-  dirDoesNotExist: (dirPath) => {
-    return this.pathDoesNotExist(dirPath);
-  },
-  fileDoesNotExist: (filePath) => {
-    return this.pathDoesNotExist(filePath);
-  },
+  }
 };
