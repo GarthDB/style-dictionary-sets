9f3045ca038474d2983e55fe78de55075e61a0d2
Garth Braithwaite <garthdb@gmail.com>
2022-06-08 14:57:44 -0600

    test: updated helper-functions tests

--- index.js
+++ index.js
@@ -2,5 +2,12 @@ const JsonSetsFormatter = require("./lib/json-sets-formatter");
 const AttributeSetsTransform = require("./lib/attribute-sets-transform");
 const NameKebabTransfom = require("./lib/name-kebab-transform");
 const CSSSetsFormatter = require("./lib/css-sets-formatter.js");
+const DroverJsonFormatter = require("./lib/drover-json-formatter.js");
 
-module.exports = { JsonSetsFormatter, AttributeSetsTransform, NameKebabTransfom, CSSSetsFormatter };
+module.exports = {
+  JsonSetsFormatter,
+  AttributeSetsTransform,
+  NameKebabTransfom,
+  CSSSetsFormatter,
+  DroverJsonFormatter,
+};
